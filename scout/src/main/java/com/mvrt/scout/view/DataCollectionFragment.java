d7d95de111508b4675752d1d41cf2fde1d874744
akalari <aditya.kalari@gmail.com>
2015-08-04 09:17:08 -0700

    Added DataCollectionFragment Abstract Class
    This class has abstract methods getData() and validate() that are
    implemented by the other fragments (auton, teleop, pregame, and
    postgame).

---
+++ scout/src/main/java/com/mvrt/scout/view/DataCollectionFragment.java
@@ -0,0 +1,16 @@
+package com.mvrt.scout.view;
+
+import android.support.v4.app.Fragment;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Aditya on 8/3/15.
+ */
+
+public abstract class DataCollectionFragment extends Fragment {
+
+    public abstract JSONObject getData();
+    public abstract boolean validate();
+
+}
